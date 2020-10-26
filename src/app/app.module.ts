50f01d929e613ad51c667a91b7f0bafc4b8f0dd3
zjschulz <zjschulz@gmail.com>
2020-10-26 14:18:48 +0000

    add directove to handle toggle of dropdown lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule
