2a3eeee9de82c8778fcf6efb4a6f3e8ee5c364c2
zjschulz <zjschulz@gmail.com>
2020-10-28 16:46:56 +0000

    add new character for testing

--- src/app/characters/character.service.ts
+++ src/app/characters/character.service.ts
@@ -12,6 +12,13 @@ export class CharacterService {
             "Cleric",
             "Forge Domain",
             "https://pbs.twimg.com/media/D2qurTfUkAIohc7?format=jpg&name=900x900"
+        ),
+        new Character(
+            "Evel",
+            "Drow",
+            "Fighter",
+            "Eldritch Knight",
+            "https://i.redd.it/k1e9e6x5za401.jpg"
         )
     ]
 
