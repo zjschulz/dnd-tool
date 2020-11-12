450d91585f630ee879a5eeb7a3c8e029d56e8771
zjschulz <zjschulz@gmail.com>
2020-11-12 13:25:35 +0000

    build out form capabilities

--- src/app/characters/character-edit/character-edit.component.ts
+++ src/app/characters/character-edit/character-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { CharacterService } from '../character.service';
 
 @Component({
   selector: 'app-character-edit',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-edit.component.css']
 })
 export class CharacterEditComponent implements OnInit {
+  id: number;
+  editMode = false;
+  characterForm: FormGroup;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private characterService: CharacterService,
+    private router: Router) { }
 
-  ngOnInit(): void {
+    private initForm() {
+      let characterName = '';
+      let characterRace = '';
+      let characterJob = '';
+      let characterSubJob = '';
+      let characterImagePath = '';
+  
+      if (this.editMode){
+        const character = this.characterService.getCharacter(this.id);
+        characterName = character.name;
+        characterRace = character.race;
+        characterJob = character.job;
+        characterSubJob = character.subjob;
+        characterImagePath = character.imagePath;
+      }
+    }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.editMode = params['id'] != null;
+          this.initForm();
+        }
+      );
   }
 
 }
