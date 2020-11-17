935dabd860652a874f562f1da98a25eba97835f3
zjschulz <zjschulz@gmail.com>
2020-11-17 13:55:38 +0000

    set up data storage service

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CharacterService } from '../characters/character.service';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient,
+                private characterService: CharacterService) {}
+}
