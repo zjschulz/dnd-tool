ef560a98b38eea970afe8126038636d6191faf9d
zjschulz <zjschulz@gmail.com>
2020-11-30 14:20:09 +0000

    add authguard to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,15 @@ import { CharacterDetailComponent } from './characters/character-detail/characte
 import { CharacterEditComponent } from './characters/character-edit/character-edit.component';
 import { CharacterStartComponent } from './characters/character-start/character-start.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/characters', pathMatch: 'full'},
-    { path: 'characters', component: CharactersComponent, children: [
+    { 
+        path: 'characters',
+        component: CharactersComponent,
+        canActivate: [AuthGuard],
+        children: [
         { path: '', component: CharacterStartComponent },
         { path: 'new', component: CharacterEditComponent },
         { path: ':id', component: CharacterDetailComponent },
