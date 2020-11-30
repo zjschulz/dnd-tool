7cdc6c8f05fe3b7554a7f5df3b8e50c23e992ed4
zjschulz <zjschulz@gmail.com>
2020-11-30 14:25:44 +0000

    add auth check on initialization of comp

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,15 +9,21 @@ import { DataStorageService } from '../shared/data-storage.service';
 })
 export class NavbarComponent implements OnInit {
   collapsed = true;
+  isAuthenticated = false;
   @Output() featureSelected = new EventEmitter<string>();
+  private userSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) { }
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature)
   }
 
   ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
   }
 
   onSaveData() {
