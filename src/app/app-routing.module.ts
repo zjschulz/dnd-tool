ed1577da88133fa2b21c33252f271ec001767e06
zjschulz <zjschulz@gmail.com>
2020-12-08 13:48:32 +0000

    set up app rputing module for new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/characters', pathMatch: 'full'},
     { path: 'home', component: HomeComponent },
-    { path: 'auth', component: AuthComponent }
+    { 
+        path: 'characters',
+        loadChildren: () => import('./characters/characters.module').then(m => m.CharactersModule)
+    },
+    { 
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    }
 ];
 
 @NgModule({
