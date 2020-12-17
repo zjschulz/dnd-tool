a1cb2e2c7a963b44ade43dbc989ad96e22121edc
zjschulz <zjschulz@gmail.com>
2020-12-17 15:12:34 +0000

    set up edit form to include item

--- src/app/characters/character-edit/character-edit.component.ts
+++ src/app/characters/character-edit/character-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { CharacterService } from '../character.service';
 
@@ -17,31 +17,50 @@ export class CharacterEditComponent implements OnInit {
     private characterService: CharacterService,
     private router: Router) { }
 
-    private initForm() {
-      let characterName = '';
-      let characterRace = '';
-      let characterJob = '';
-      let characterSubJob = '';
-      let characterImagePath = '';
-  
-      if (this.editMode){
-        const character = this.characterService.getCharacter(this.id);
-        characterName = character.name;
-        characterRace = character.race;
-        characterJob = character.job;
-        characterSubJob = character.subjob;
-        characterImagePath = character.imagePath;
+  get controls() {
+    return (<FormArray>this.characterForm.get('items')).controls;
+  }
+
+  private initForm() {
+    let characterName = '';
+    let characterRace = '';
+    let characterJob = '';
+    let characterSubJob = '';
+    let characterImagePath = '';
+    let characterItems = new FormArray([]);
+
+    if (this.editMode){
+      const character = this.characterService.getCharacter(this.id);
+      characterName = character.name;
+      characterRace = character.race;
+      characterJob = character.job;
+      characterSubJob = character.subjob;
+      characterImagePath = character.imagePath;
+      if (character['ingredients']) {
+        for (let item of character.items) {
+          characterItems.push(
+            new FormGroup({
+              'name': new FormControl(item.name, Validators.required),
+              'amount': new FormControl(item.weight, [
+                Validators.required,
+                Validators.pattern(/^[1-9]+[0-9]*$/)
+              ])
+            })
+          );
+        }
       }
-
-      this.characterForm = new FormGroup({
-        'name': new FormControl(characterName, Validators.required),
-        'race': new FormControl(characterRace, Validators.required),
-        'job': new FormControl(characterJob, Validators.required),
-        'subjob': new FormControl(characterSubJob, Validators.required),
-        'imagePath': new FormControl(characterImagePath, Validators.required)
-      });
-
     }
+
+    this.characterForm = new FormGroup({
+      'name': new FormControl(characterName, Validators.required),
+      'race': new FormControl(characterRace, Validators.required),
+      'job': new FormControl(characterJob, Validators.required),
+      'subjob': new FormControl(characterSubJob, Validators.required),
+      'imagePath': new FormControl(characterImagePath, Validators.required),
+      'items': characterItems
+    });
+
+  }
 
   ngOnInit() {
     this.route.params
@@ -66,6 +85,22 @@ export class CharacterEditComponent implements OnInit {
 
   onCancel() {
     this.router.navigate(['../'], {relativeTo: this.route})
+  }
+
+  onAddItem() {
+    (<FormArray>this.characterForm.get('items')).push(
+      new FormGroup({
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [
+          Validators.required,
+          Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
+      })
+    )
+  }
+
+  onDeleteItem(index: number) {
+    (<FormArray>this.characterForm.get('items')).removeAt(index);
   }
 
 }
