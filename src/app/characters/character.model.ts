e0e6af5191b76cf9bbe93da3edda71b96d37f196
zjschulz <zjschulz@gmail.com>
2020-12-17 14:59:39 +0000

    import item model to character model

--- src/app/characters/character.model.ts
+++ src/app/characters/character.model.ts
@@ -1,16 +1,20 @@
+import { Item } from '../shared/item.model';
+
 export class Character {
     public name: string;
     public race: string;
     public job: string;
     public subjob: string;
     public imagePath: string;
-    
-    constructor(name: string, race: string, job: string, subjob: string, imagePath: string) {
+    public items: Item[];
+
+    constructor(name: string, race: string, job: string, subjob: string, imagePath: string, items: Item[]) {
         this.name = name;
         this.race = race;
         this.job = job;
         this.subjob = subjob;
         this.imagePath = imagePath;
+        this.items = items;
     }
     
 }
