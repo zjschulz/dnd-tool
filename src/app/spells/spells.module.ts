8ee65064badc483b42d07b9314e920d8710a5515
zjschulz <zjschulz@gmail.com>
2020-12-19 16:02:49 +0000

    create spells components

--- src/app/spells/spells.module.ts
+++ src/app/spells/spells.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+import { SpellsComponent } from './spells.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        SpellsComponent
+    ],
     imports: [],
-    exports: []
+    exports: [
+        SpellsComponent
+    ]
 })
 export class SpellsModule {}
