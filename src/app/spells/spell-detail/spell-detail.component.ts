4abd7d49a0c00d9a3c2e9fbd033a4da0fd595ae1
zjschulz <zjschulz@gmail.com>
2020-12-22 17:59:53 +0000

    show spell deatils upon click

--- src/app/spells/spell-detail/spell-detail.component.ts
+++ src/app/spells/spell-detail/spell-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit} from '@angular/core';
+import { Spell } from '../spell.model';
+import { SpellService } from '../spell.service';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-spell-detail',
@@ -6,9 +9,22 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./spell-detail.component.css']
 })
 export class SpellDetailComponent implements OnInit {
+  spell: Spell;
+  id: number;
 
-  constructor() { }
+  constructor(private spellService: SpellService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    debugger;
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.spell = this.spellService.getSpell(this.id);
+      }
+    )
+  }
 
 }
