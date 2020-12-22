4daf29968e66e4f872a56e66f6810f37abc5c908
zjschulz <zjschulz@gmail.com>
2020-12-22 14:23:54 +0000

    create fetch request for spell list data

--- src/app/spells/spells.component.ts
+++ src/app/spells/spells.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Spell } from './spell.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-spells',
@@ -7,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpellsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
 
+  }
+
+  onFetchSpells() {
+    return this.http.get(
+      'https://www.dnd5eapi.co/api/spells'
+    )
+    .subscribe(res => console.log(res.results))
+    // get list of spell index's
+    // create list component and have this fetch request populate the list
+    // create detail component that fetch spell data on clicked spell in list
   }
 
 }
