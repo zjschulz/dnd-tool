a7aee2cb69b9144ca73c7296dc091dded87f67c2
zjschulz <zjschulz@gmail.com>
2021-01-05 13:25:27 +0000

    build out race start comp

---
+++ src/app/races/race-start/race-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-race-start',
+  templateUrl: './race-start.component.html',
+  styleUrls: ['./race-start.component.css']
+})
+export class RaceStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
