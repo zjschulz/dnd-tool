6accdd2bc93f166b2403d3aeb527d0598af578ae
zjschulz <zjschulz@gmail.com>
2021-01-05 13:46:41 +0000

    build out race detail comps

---
+++ src/app/races/race-detail/race-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
+import { Race } from '../race.model';
+import { RaceService } from '../race.service';
+
+@Component({
+  selector: 'app-race-detail',
+  templateUrl: './race-detail.component.html',
+  styleUrls: ['./race-detail.component.css']
+})
+export class RaceDetailComponent implements OnInit {
+  race: Race;
+  id: number;
+  data: any;
+
+  constructor(private raceService: RaceService,
+              private route: ActivatedRoute,
+              private dataStorageService: DataStorageService) { }
+
+  ngOnInit() {
+    this.route.params
+    .subscribe( 
+      (params: Params) => {
+        this.id = +params['id'];
+        this.race = this.raceService.getRace(this.id);
+        this.dataStorageService.fetchSpell(this.race.url)
+        .subscribe((res: any) => {
+          console.log(res);
+          this.data = res;
+        });
+      }
+    )
+  }
+
+}
