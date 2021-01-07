227285d2cae9e86eb44c9e24210972af67cb2e09
zjschulz <zjschulz@gmail.com>
2021-01-07 18:50:33 +0000

    create and build monster start comp

---
+++ src/app/monsters/monster-start/monster-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monster-start',
+  templateUrl: './race-monster.component.html',
+  styleUrls: ['./race-monster.component.css']
+})
+export class MonsterStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
