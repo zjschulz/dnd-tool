7c142caaffab7593a437f983db7557ebba40b54e
zjschulz <zjschulz@gmail.com>
2021-01-07 18:58:43 +0000

    create and build out monster detail comp

---
+++ src/app/monsters/monster-detail/monster-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
+import { Monster } from '../monster.model';
+import { MonsterService } from '../monster.service';
+
+@Component({
+  selector: 'app-monster-detail',
+  templateUrl: './monster-detail.component.html',
+  styleUrls: ['./monster-detail.component.css']
+})
+export class MonsterDetailComponent implements OnInit {
+  monster: Monster;
+  id: number;
+  data: any;
+
+  constructor(private monsterService: MonsterService,
+              private route: ActivatedRoute,
+              private dataStorageService: DataStorageService) { }
+
+  ngOnInit() {
+    this.route.params
+    .subscribe( 
+      (params: Params) => {
+        this.id = +params['id'];
+        this.monster = this.monsterService.getMonster(this.id);
+        this.dataStorageService.fetchUrl(this.monster.url)
+        .subscribe((res: any) => {
+          console.log(res);
+          this.data = res;
+        });
+      }
+    )
+  }
+
+}
