8cc79dc45d663fc19d0d388c8c74b98cdc02dc6e
zjschulz <zjschulz@gmail.com>
2021-01-07 18:39:22 +0000

    build out monsters component and import to routing module

--- src/app/monsters/monsters-routing.module.ts
+++ src/app/monsters/monsters-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MonstersComponent } from './monsters.component';
 
 const routes: Routes = [
     { 
---
+++ src/app/monsters/monsters.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Component({
+  selector: 'app-monsters',
+  templateUrl: './monsters.component.html',
+  styleUrls: ['./monsters.component.css']
+})
+export class MonstersComponent implements OnInit {
+
+  constructor(private dataStorageService: DataStorageService) { }
+
+  ngOnInit() {
+    
+  }
+
+  onFetchRaces() {
+    this.dataStorageService.fetchMonsters().subscribe();
+  }
+
+}
