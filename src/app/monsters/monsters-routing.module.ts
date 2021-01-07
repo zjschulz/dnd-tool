29982e1ef3a033d6c1efa7cc06dc67ed788d2e0f
zjschulz <zjschulz@gmail.com>
2021-01-07 19:01:01 +0000

    update routes with created comps

--- src/app/monsters/monsters-routing.module.ts
+++ src/app/monsters/monsters-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MonstersComponent } from './monsters.component';
+import { MonsterStartComponent } from './monster-start/monster-start.component';
+import { MonsterDetailComponent } from './monster-detail/monster-detail.component';
 
 const routes: Routes = [
     { 
         path: '',
-        component: MonstersComponent
-    //     children: [
-    //     { path: '', component: MonsterStartComponent },
-    //     { path: ':id', component: MonsterDetailComponent }
-    // ] 
+        component: MonstersComponent,
+        children: [
+        { path: '', component: MonsterStartComponent },
+        { path: ':id', component: MonsterDetailComponent }
+    ] 
     }
 ]
 
