b312207ae05d4d358b1aa8b43d9e7fc78f29acfc
zjschulz <zjschulz@gmail.com>
2021-01-28 14:40:25 +0000

    build out switch functionality in ngOnInit

--- src/app/items/item-detail/item-detail.component.ts
+++ src/app/items/item-detail/item-detail.component.ts
@@ -13,6 +13,16 @@ export class ItemDetailComponent implements OnInit{
   item: Item;
   index: string;
   data: any;
+  isWeapon: boolean;
+  isArmor: boolean;
+  isStandardGear: boolean;
+  isEquipmentPack: boolean;
+  isTool: boolean;
+  isHolySymbol: boolean;
+  isDruidicFoci: boolean;
+  isArcaneFoci: boolean;
+  isMountsAndVehicles: boolean;
+
 
   constructor(private itemService: ItemService,
               private route: ActivatedRoute,
@@ -28,6 +38,119 @@ export class ItemDetailComponent implements OnInit{
                     .subscribe((res: any) => {
                       console.log(res);
                       this.data = res;
+                      switch(res.equipment_category.name) {
+                        case "Weapon":
+                          this.isWeapon = true;
+                          this.isArmor = false;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = false;
+                          this.isHolySymbol = false;
+                          this.isDruidicFoci = false;
+                          this.isArcaneFoci = false;
+                          this.isMountsAndVehicles = false;
+                          break;
+                        case "Armor":
+                          this.isWeapon = false;
+                          this.isArmor = true;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = false;
+                          this.isHolySymbol = false;
+                          this.isDruidicFoci = false;
+                          this.isArcaneFoci = false;
+                          this.isMountsAndVehicles = false;
+                          break;
+                        case "Adventuring Gear":
+                          if (res.gear_category.name === "Standard Gear") {
+                            this.isWeapon = false;
+                            this.isArmor = false;
+                            this.isStandardGear = true;
+                            this.isEquipmentPack = false;
+                            this.isTool = false;
+                            this.isHolySymbol = false;
+                            this.isDruidicFoci = false;
+                            this.isArcaneFoci = false;
+                            this.isMountsAndVehicles = false;
+                          }
+                          else if (res.gear_category.name === "Equipment Packs") {
+                            this.isWeapon = false;
+                            this.isArmor = false;
+                            this.isStandardGear = false;
+                            this.isEquipmentPack = true;
+                            this.isTool = false;
+                            this.isHolySymbol = false;
+                            this.isDruidicFoci = false;
+                            this.isArcaneFoci = false;
+                            this.isMountsAndVehicles = false;
+                          }
+                          break;
+                        case "Tools":
+                          this.isWeapon = false;
+                          this.isArmor = false;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = true;
+                          this.isHolySymbol = false;
+                          this.isDruidicFoci = false;
+                          this.isArcaneFoci = false;
+                          this.isMountsAndVehicles = false;
+                          break;  
+                        case "Holy Symbol":
+                          this.isWeapon = false;
+                          this.isArmor = false;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = false;
+                          this.isHolySymbol = true;
+                          this.isDruidicFoci = false;
+                          this.isArcaneFoci = false;
+                          this.isMountsAndVehicles = false;
+                          break; 
+                        case "Druidic Foci":
+                          this.isWeapon = false;
+                          this.isArmor = false;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = false;
+                          this.isHolySymbol = false;
+                          this.isDruidicFoci = true;
+                          this.isArcaneFoci = false;
+                          this.isMountsAndVehicles = false;
+                          break; 
+                        case "Arcane Foci":
+                          this.isWeapon = false;
+                          this.isArmor = false;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = false;
+                          this.isHolySymbol = false;
+                          this.isDruidicFoci = false;
+                          this.isArcaneFoci = true;
+                          this.isMountsAndVehicles = false;
+                          break; 
+                        case "Mounts and Vehicles":
+                          this.isWeapon = false;
+                          this.isArmor = false;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = false;
+                          this.isHolySymbol = false;
+                          this.isDruidicFoci = false;
+                          this.isArcaneFoci = false;
+                          this.isMountsAndVehicles = true;
+                          break;   
+                        default:
+                          this.isWeapon = false;
+                          this.isArmor = false;
+                          this.isStandardGear = false;
+                          this.isEquipmentPack = false;
+                          this.isTool = false;
+                          this.isHolySymbol = false;
+                          this.isDruidicFoci = false;
+                          this.isArcaneFoci = false;
+                          this.isMountsAndVehicles = false;
+                      }
                     });
                   }
                 )
