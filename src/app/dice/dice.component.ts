a3e4e65b6710ab0361c395cc95e33e2ee38e2ca4
zjschulz <zjschulz@gmail.com>
2021-03-04 16:13:57 +0000

    set up button to initiate random rolls

--- src/app/dice/dice.component.ts
+++ src/app/dice/dice.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dice.component.css']
 })
 export class DiceComponent implements OnInit {
+  results: number;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  rollDice(max) {
+    return 1 + Math.floor(Math.random() * max)
+  }
+
+  onSubmit(){
+    this.results = this.rollDice(4) + this.rollDice(6) + this.rollDice(8) + this.rollDice(10) + this.rollDice(12) + this.rollDice(20) + this.rollDice(100)
+    console.log(this.results)
   }
 
 }
