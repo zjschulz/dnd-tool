6f20c4b67962dc075347a05757d39b0c48f50aa8
zjschulz <zjschulz@gmail.com>
2021-03-04 14:09:12 +0000

    create diceroller components

---
+++ src/app/diceroller/diceroller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diceroller',
+  templateUrl: './diceroller.component.html',
+  styleUrls: ['./diceroller.component.css']
+})
+export class DicerollerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
